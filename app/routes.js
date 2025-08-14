079fd796bbc51671ef69224c769161faeb0e0f95
knowdigital <knowdigital@users.noreply.github.com>
2025-08-14 13:14:36 +0100

    update

---
+++ app/routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = new express.Router()
+
+// Add your routes here - above the module.exports line
+
+// GET SPRINT NAME - useful for relative templates
+
+// route middleware that will happen on every request
+router.use('/', (req, res, next) => {
+     res.locals.currentURL = req.originalUrl; //current screen
+     res.locals.prevURL = req.get('Referrer'); // previous screen
+     console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );
+     next();
+});
+
+///////////////////////////////////////// New router functionality /////////////////////////////////////////
+
+// User Research and design versions
+router.use('/version-0', require('./views/version-0/_routes'))
+router.use('/version-1', require('./views/version-1/_routes'))
+router.use('/version-2', require('./views/version-2/_routes'))
+router.use('/version-3', require('./views/version-3/_routes'))
+router.use('/version-4', require('./views/version-4/_routes'))
+router.use('/version-5', require('./views/version-5/_routes'))
+router.use('/version-6', require('./views/version-6/_routes'))
+router.use('/version-7', require('./views/version-7/_routes'))
+
+module.exports = router
