5ab7fdc360b1053c49afca11c50c4eba07ef8a36
monicasara <monicasantos.ux@gmail.com>
2025-08-20 10:04:22 +0100

    Add suspects journey in progress

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -24,6 +24,7 @@ module.exports = {
   "employer.name": "Assurance Aerospace Engineering",
   "employer.id": "RVRD8V",
   "suspectCount": 0,
+  "suspectType": [],
   "suspectFirstName": [],
   "suspectLastName": [],
   "suspectCompanyName": []
