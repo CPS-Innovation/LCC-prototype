4a0d4d639776b4c2667786919543b357ce742cca
monicasara <72076797+monicasara@users.noreply.github.com>
2025-08-22 16:59:33 +0100

    Changes on the register a case journey (#40)
    * New fork
    
    * Update to index file
    
    * Test commit
    
    * Changes to register a case journey to swap case details around
    
    * Changes to register a case journey

---
+++ app/assets/javascripts/autocomplete/docType-Area.js
@@ -0,0 +1,67 @@
+$(function() {
+    var areas = [
+        { value: "SEOCID Int London and SE Div" },
+        { value: "SEOCID Regional and Wales Div" },
+        { value: "Counter Terrorism Division" },
+        { value: "Special Crime Division" },
+        { value: "Specialist Fraud Division" },
+        { value: "Avon & Somerset" },
+        { value: "Bedfordshire" },
+        { value: "Cambridgeshire" },
+        { value: "Cheshire" },
+        { value: "Cleveland" },
+        { value: "Cumbria" },
+        { value: "Derbyshire" },
+        { value: "Devon and Cornwall" },
+        { value: "Dorset" },
+        { value: "Durham" },
+        { value: "Dyfed Powys" },
+        { value: "Essex" },
+        { value: "Gloucestershire" },
+        { value: "Greater Manchester" },
+        { value: "Gwent" },
+        { value: "Hampshire & IOW" },
+        { value: "Hertfordshire" },
+        { value: "HMCPSI" },
+        { value: "Humberside" },
+        { value: "Kent" },
+        { value: "Lancashire" },
+        { value: "Leatherhead" },
+        { value: "Leicestershire" },
+        { value: "Lincolnshire" },
+        { value: "London" },
+        { value: "London North" },
+        { value: "London South" },
+        { value: "Merseyside" },
+        { value: "Norfolk" },
+        { value: "North Wales" },
+        { value: "North Yorkshire" },
+        { value: "Northamptonshire" },
+        { value: "Northumbria" },
+        { value: "Nottinghamshire" },
+        { value: "Records Management Team (RMT)" },
+        { value: "RMU Area" },
+        { value: "South Wales" },
+        { value: "South Yorkshire" },
+        { value: "Stafford" },
+        { value: "Suffolk" },
+        { value: "Surrey" },
+        { value: "Sussex" },
+        { value: "Thames Valley" },
+        { value: "Warwickshire" },
+        { value: "West Mercia" },
+        { value: "West Midlands" },
+        { value: "West Yorkshire" },
+        { value: "Wilstshire" }
+    ];
+
+     // setup autocomplete function pulling from area[] array
+     $('#docType-Area').autocomplete({
+          lookup: areas,
+          onSelect: function (suggestion) {
+               var thehtml = '<strong>Area:</strong> ' + suggestion.value + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
+               $('#docType-Area').html(thehtml);
+          }
+     });
+
+});
